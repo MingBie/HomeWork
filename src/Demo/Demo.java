0e3a5e46e1d16768f1233f4b270005ef5fbb9b44
呆自信丶 <389395995@qq.com>
2020-08-12 22:17:48 +0800

    2020.8.12

--- src/Demo/Demo.java
+++ src/Demo/Demo.java
@@ -15,12 +15,12 @@ public class Demo {
             String s1;
             StringBuilder sb1 = new StringBuilder(); // 存储从 a.txt 读取的单词
             String s2;
-            StringBuilder sb2 = new StringBuilder(); // // 存储从 b.txt 读取的单词
+            StringBuilder sb2 = new StringBuilder(); // 存储从 b.txt 读取的单词
             // 读取 a.txt 中的单词
             while ((s1 = br1.readLine()) != null) {
                 sb1.append(s1 + " "); // 以空格 隔开
             }
-            // 读取 a.txt 中的单词
+            // 读取 .txt 中的单词
             while ((s2 = br2.readLine()) != null) {
                 sb2.append(s2 + " "); // 以空格 隔开
             }
@@ -35,7 +35,7 @@ public class Demo {
                 length = ss2.length;
             }
             // 交替添加 并以空格隔开
-            // 先添加相等长度的单词
+            // 先添加相等长度的单词（短单词数组的长度）
             StringBuilder s = new StringBuilder();
             for(int i = 0; i < length; i++) {
                 s.append(ss1[i] + " " + ss2[i]);
